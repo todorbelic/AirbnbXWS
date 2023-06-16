8f6e53841aba7f3d36baec236be62d693151582e
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2023-06-16 23:01:34 +0200

    ubcssss

--- backend/NotificationService/Program.cs
+++ backend/NotificationService/Program.cs
@@ -1,4 +1,10 @@
 using NotificationService.Services;
+using UserService.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NotificationService.Repository;
+using NotificationService.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +12,47 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
 
+builder.Services.AddGrpc(options =>
+{
+    options.Interceptors.Add<ErrorHandlingInterceptor>();
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false, // Allow all issuers
+                ValidateAudience = false, // Allow all audiences
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        });
+
+builder.Services.AddScoped<IAppNotificationService, AppNotificationService>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+//builder.Services.AddTransient<ExceptionMiddleware>();
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+builder.Services.AddSingleton<IMongoDbSettings>(serviceProvider =>
+       serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+builder.Services.Configure<MongoDbSettings>(configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.UseAuthentication();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    //endpoints.MapGrpcService<UserHandler>();
+});
 
 app.Run();
+
+
