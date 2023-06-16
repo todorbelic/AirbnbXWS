b198356b13d159181c4a902ec014100ebd777cfd
todorbelic <belictodor@gmail.com>
2023-06-16 21:02:00 +0200

    review service

---
+++ backend/ReviewService/Model/Rating.cs
@@ -0,0 +1,18 @@
+﻿namespace ReviewService.Model
+{
+    public class Rating : Neo4jRelationship
+    {
+        public double Value { get; set; }
+        public DateOnly Date { get; set; }
+
+        public DateTimeOffset Created { get; set; }
+
+        public Rating()
+        {
+            Name = "RATE";
+            Created = DateTimeOffset.UtcNow;
+        }
+
+
+    }
+}
