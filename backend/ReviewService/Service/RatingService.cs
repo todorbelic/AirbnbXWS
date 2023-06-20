dbe23a606e61c011d1e3ba66ad86abd436fa7be6
DanaBrasanac <brasanac.ra239.2018@uns.ac.rs>
2023-06-20 11:34:08 +0200

    can guest rate..

--- backend/ReviewService/Service/RatingService.cs
+++ backend/ReviewService/Service/RatingService.cs
@@ -1,5 +1,7 @@
-﻿using ReviewService.Model;
+﻿using Grpc.Net.Client;
+using ReviewService.Model;
 using ReviewService.Repository;
+using System.Globalization;
 
 namespace ReviewService.Service
 {
@@ -14,12 +16,31 @@ namespace ReviewService.Service
 
         public async Task RateAccomodation(RateAccommodationRequest request)
         {
+            if (!CreateCanGuestRateAccommodationRequest(request.AccommodationId, request.GuestId)) throw new Exception("guest cannot rate this accommodation!");
             Rating rating = new Rating() { Value = (int)request.Rating };
             await _guestRepository.Relate<Accommodation, Rating>($"{{guestId :'{request.GuestId}'}}", $"{{accommodationId :'{request.AccommodationId}'}}", rating, DateTime.Now, rating.Value);
         }
 
+        
+        private bool CreateCanGuestRateAccommodationRequest(string accommodationId, string guestId)
+        {
+            CanGuestRateAccommodationRequest request = new CanGuestRateAccommodationRequest() { GuestId = guestId, AccommodationId = accommodationId };
+            var channel = GrpcChannel.ForAddress("http://reservation_service:8080");
+            var client = new InternalReservationServiceRPC.InternalReservationServiceRPCClient(channel);
+            return client.CanGuestRateAccommodation(request).Response;
+        }
+
+        private bool CreateCanGuestRateHostRequest(string hostId, string guestId)
+        {
+            CanGuestRateHostRequest request = new CanGuestRateHostRequest() { HostId = hostId, GuestId = guestId };
+            var channel = GrpcChannel.ForAddress("http://reservation_service:8080");
+            var client = new InternalReservationServiceRPC.InternalReservationServiceRPCClient(channel);
+            return client.CanGuestRateHost(request).Response;
+        }
+        
         public async Task RateHost(RateHostRequest request)
         {
+            if (!CreateCanGuestRateHostRequest(request.HostId, request.GuestId)) throw new Exception("guest cannot rate this host!");
             Rating rating = new Rating() { Value = (int)request.Rating };
             await _guestRepository.Relate<Accommodation, Rating>($"{{guestId :'{request.GuestId}'}}", $"{{hostId :'{request.HostId}'}}", rating, DateTime.Now, rating.Value);
         }
